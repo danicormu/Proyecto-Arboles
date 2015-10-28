cb462f1d34719875be2d70f1ef0bd6966db5859d
danicormu <danicormu@gmail.com>
2015-10-28 09:01:43 -0600

    some features

--- ProyectoArboles/NodoA.cpp
+++ ProyectoArboles/NodoA.cpp
@@ -3,9 +3,40 @@
 
 NodoA::NodoA(void)
 {
+	setDato(0);
+	setIzq(NULL);
+	setDer(NULL);
 }
 
 
 NodoA::~NodoA(void)
 {
+}
+
+NodoA::NodoA(int _dato){
+	
+	setDato(_dato);
+	setIzq(NULL);
+	setDer(NULL);
+}
+int NodoA::getDato(){
+		return this ->dato;
+	}
+void NodoA::setDato(int _dato){
+	this ->dato = _dato;
+
+}
+
+NodoA *&NodoA::getIzq(){
+	return this -> izq;
+}
+void NodoA::setIzq(NodoA * _ptr){
+		this ->izq = _ptr;
+}
+
+NodoA *&NodoA::getDer(){
+		return this -> der;
+}
+void NodoA::setDer(NodoA * _ptr){
+		this->der = _ptr;
 }
--- ProyectoArboles/NodoA.h
+++ ProyectoArboles/NodoA.h
@@ -3,9 +3,21 @@
 class NodoA
 {
 private:
-
+	int dato;
+	NodoA * izq;
+	NodoA * der;
 public:
 	NodoA(void);
 	~NodoA(void);
+
+	NodoA(int);
+	int getDato();
+	void setDato(int);
+
+	NodoA *&getIzq();
+	void setIzq(NodoA *);
+
+	NodoA *&getDer();
+	void setDer(NodoA *);
 };
 
