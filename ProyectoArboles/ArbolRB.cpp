d986c4e846997f8f1841531253e15ff8ab33e9b5
danicormu <danicormu@gmail.com>
2015-11-07 15:21:24 -0600

    Some changes

--- ProyectoArboles/ArbolRB.cpp
+++ ProyectoArboles/ArbolRB.cpp
@@ -10,6 +10,8 @@ ArbolRB::~ArbolRB(void)
 {
 }
 
+/* ------ Metodos privados ------ */
+
 //Metodo de rotacion a la izquierda
 void ArbolRB::leftRotate(NodoRB * _x){
 	NodoRB * _y;
@@ -45,3 +47,54 @@ void ArbolRB::rightRotate(NodoRB * _y){
 		_x->getDer() = _y;
 		_y->getPar() = _x;
 }
+/*
+void TreeInsertHelp(NodoRB * _z){
+	NodoRB * x;
+	NodoRB * y;
+
+	//Si no es NULL es nil
+	_z->getIzq() = _z->getDer() = NULL; 
+	y = raiz;
+	x = raiz->getIzq();
+
+	while(x != NULL){
+		y = x;
+		if ( x->key > _z->key) { 
+			x = x-> getIzq();
+		} else {
+			x = x-> getDer();
+		 }
+	}
+	_z->getPar() = y;
+	if ( (y == raiz) || (y->key > _z->key) ) { 
+		y->getIzq = _z;
+	} else {
+	    y->getDer() = _z;
+	}
+}
+*/
+
+
+
+/* ------ Metodos publicos ------ */
+
+//Metodo de insercion
+/*
+NodoRB * ArbolRB::insert(ArbolRB * _new){
+	NodoRB * y;
+	NodoRB * x;
+	NodoRB * newNodo;
+
+	x = new NodoRB(_new);
+	TreeInsertHelp(x);
+
+	newNodo = x;
+	x->
+	
+
+
+	
+
+}*/
+NodoRB * ArbolRB::search(int key){}
+ArbolRB * ArbolRB::deleteNode(NodoRB *){}
--- ProyectoArboles/ProyectoArboles.cpp
+++ ProyectoArboles/ProyectoArboles.cpp
@@ -4,22 +4,103 @@
 #include "stdafx.h"
 
 void Menu();
+void menuSecudario();
 
 void Menu(){
 	
 	int opc = 0;
 	do{
-		cout << "Proyecto de Arboles "<< endl;	
-		cout << "( 1 ) "<< endl;
-		cout << "( 2 ) "<< endl;
-		cout << "( 3 ) "<< endl;
-		cout << "( 4 ) "<< endl;
-		cout << "( 5 ) "<< endl;
+		cout << "---- Arbol Rojo y Negro ----"<< endl;	
+		cout << "( 1 ) Menu Secundario "<< endl;
+		cout << "( 2 ) Insertar"<< endl;
+		cout << "( 3 ) Buscar"<< endl;
+		cout << "( 4 ) Eliminar"<< endl;
+		cout << "( 5 ) Rotaciones"<< endl;
+		cout << "( 0 ) Salir ---->"<< endl;
 		
+		switch (opc)
+		{
+		case 1: {
+				menuSecudario();
+				break;
+				}
+				
+		case 2: {
+				
+			cout << "Insercion " << endl;
+			break;
+			}
+
+		case 3: {
+
+			cout << "Buscar" << endl;
+			break;
+			}
+
+		case 4: {
+
+			cout << "Eliminar " << endl;
+			break;
+			}
+
+		case 5: {
+			cout << "Rotaciones " << endl;
+			break;
+			}
+
+
+		default:
+			cout << "Opcion invalida" << endl; 
+			break;
+		}
 	system("pause");
 	}while(opc != 0);
+}
 
 
+void menuSecudario(){
+
+	int opc;
+	do{
+		cout << "---- Menu secundario ----"<< endl;	
+		cout << "---- Verificar el arbol ----"<< endl;	
+		cout << "( 1 ) Lleno "<< endl;
+		cout << "( 2 ) Completo"<< endl;
+		cout << "( 3 ) Balanceado"<< endl;
+		cout << "( 4 ) Totalmente balanceado"<< endl;
+		cout << "( 0 ) Salir ---->"<< endl;
+	
+		switch (opc)
+		{
+		case 1:{
+				cout << "Lleno" << endl;
+
+			   break;
+			   }
+
+		case 2:{
+				cout << "Completo" << endl;
+
+			   break;
+			   }
+
+		case 3:{
+				cout << "Balanceado" << endl;
+
+			   break;
+			   }
+
+		case 4:{
+				cout << "Totalmente balanceado" << endl;
+
+			   break;
+			   }
+		default:
+			cout << "Opcion invalida" << endl; 
+			break;
+		}
+	}
+	while(opc != 0);
 
 }
 int _tmain(int argc, _TCHAR* argv[])
